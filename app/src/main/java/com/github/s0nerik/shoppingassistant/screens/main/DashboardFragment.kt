0248f13146c36f284fca5449f9b8aabdfac41334
Alex Isaienko <thesonerik@gmail.com>
2016-12-27 22:00:24 +0200

    Added a sample chart

--- app/src/main/java/com/github/s0nerik/shoppingassistant/screens/main/DashboardFragment.kt
+++ app/src/main/java/com/github/s0nerik/shoppingassistant/screens/main/DashboardFragment.kt
@@ -1,7 +1,12 @@
 package com.github.s0nerik.shoppingassistant.screens.main
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.View
+import com.github.mikephil.charting.components.Legend
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
 import com.github.nitrico.lastadapter.LastAdapter
 import com.github.s0nerik.shoppingassistant.BR
 import com.github.s0nerik.shoppingassistant.R
@@ -11,7 +16,9 @@ import com.github.s0nerik.shoppingassistant.databinding.FragmentDashboardBinding
 import com.github.s0nerik.shoppingassistant.model.Purchase
 import io.realm.PurchaseRealmProxy
 import io.realm.Realm
+import io.realm.RealmResults
 import kotlinx.android.synthetic.main.fragment_dashboard.*
+import org.jetbrains.anko.support.v4.act
 
 /**
  * Created by Alex on 12/25/2016.
@@ -20,16 +27,19 @@ import kotlinx.android.synthetic.main.fragment_dashboard.*
  */
 class DashboardFragment : BaseBoundFragment<FragmentDashboardBinding>(R.layout.fragment_dashboard) {
 
-    private lateinit var realm: Realm
+    private val realm: Realm by lazy { Realm.getDefaultInstance() }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        realm = Realm.getDefaultInstance()
+    private val purchases: RealmResults<Purchase> by lazy {
+        realm.where(Purchase::class.java).findAll()
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val purchases = realm.where(Purchase::class.java).findAll()
+        initRecents()
+        initDistributionChart()
+    }
+
+    private fun initRecents() {
         LastAdapter.with(purchases, BR.item)
                 .map<PurchaseRealmProxy>(R.layout.item_recent_purchases)
                 .into(recentsRecycler)
@@ -38,6 +48,33 @@ class DashboardFragment : BaseBoundFragment<FragmentDashboardBinding>(R.layout.f
         recentsRecycler.setHasFixedSize(true)
 
         scrollView.applyWrongNestedScrollWorkaround()
+    }
+
+    private fun initDistributionChart() {
+        val dataSet = PieDataSet(listOf(PieEntry(10f, "Food"), PieEntry(10f, "Clothes"), PieEntry(10f, "Instruments")), null)
+        dataSet.apply {
+            setColors(intArrayOf(R.color.colorPrimary, R.color.colorPrimaryDark, R.color.colorAccent), act)
+            sliceSpace = 4f
+            valueTextColor = ContextCompat.getColor(act, R.color.material_color_white)
+            valueTextSize = 24f
+        }
+
+        distributionChart.apply {
+            data = PieData(dataSet)
+            description = null
+            holeRadius = 5f
+            transparentCircleRadius = 10f
+
+            legend.apply {
+                isEnabled = true
+                orientation = Legend.LegendOrientation.VERTICAL
+                direction = Legend.LegendDirection.LEFT_TO_RIGHT
+                horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
+                verticalAlignment = Legend.LegendVerticalAlignment.CENTER
+                textSize = 16f
+                setDrawInside(false)
+            }
+        }
     }
 
     override fun onDestroy() {
