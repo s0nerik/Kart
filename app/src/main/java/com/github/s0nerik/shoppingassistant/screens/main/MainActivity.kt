2b444e945ec6a098065d641fbd4e420f37c58d5b
Alex Isaienko <thesonerik@gmail.com>
2017-02-23 01:43:57 +0200

    Added a cart menu item badge.

--- app/src/main/java/com/github/s0nerik/shoppingassistant/screens/main/MainActivity.kt
+++ app/src/main/java/com/github/s0nerik/shoppingassistant/screens/main/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.github.s0nerik.shoppingassistant.screens.main
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.MenuItem
+import berlin.volders.badger.BadgeShape
+import berlin.volders.badger.Badger
+import berlin.volders.badger.CountBadge
 import com.github.s0nerik.shoppingassistant.R
 import com.github.s0nerik.shoppingassistant.base.BaseBoundActivity
 import com.github.s0nerik.shoppingassistant.databinding.ActivityMainBinding
@@ -35,5 +39,11 @@ class MainActivity : BaseBoundActivity<ActivityMainBinding>(R.layout.activity_ma
         supportFragmentManager.beginTransaction()
                 .replace(R.id.container, DashboardFragment())
                 .commit()
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+        val badge = Badger.sett(binding.bottomNavigation.menu.getItem(2), CountBadge.Factory(this, BadgeShape.circle(0.5f, Gravity.RIGHT.or(Gravity.TOP))))
+        badge.setCount(12)
     }
 }
