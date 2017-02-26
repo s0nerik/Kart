86acedbe661185b192cef7ef3d2fe58d2f8af276
Alex Isaienko <thesonerik@gmail.com>
2017-02-26 04:05:07 +0200

    Added a listener for setting a proper badge.

--- app/src/main/java/com/github/s0nerik/shoppingassistant/screens/main/MainActivity.kt
+++ app/src/main/java/com/github/s0nerik/shoppingassistant/screens/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.github.s0nerik.shoppingassistant.screens.main
 
+import android.databinding.ObservableList
 import android.os.Bundle
 import android.view.Gravity
 import android.view.MenuItem
@@ -8,7 +9,9 @@ import berlin.volders.badger.Badger
 import berlin.volders.badger.CountBadge
 import com.github.s0nerik.shoppingassistant.R
 import com.github.s0nerik.shoppingassistant.base.BaseBoundActivity
+import com.github.s0nerik.shoppingassistant.currentCart
 import com.github.s0nerik.shoppingassistant.databinding.ActivityMainBinding
+import com.github.s0nerik.shoppingassistant.model.Purchase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivityViewModel(private val activity: MainActivity) {
@@ -35,6 +38,17 @@ class MainActivityViewModel(private val activity: MainActivity) {
 }
 
 class MainActivity : BaseBoundActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private lateinit var badge: CountBadge
+
+    // TODO: clear all listeners of the list in cart when cart gets saved.
+    private val cartChangeListener = object: ObservableList.OnListChangedCallback<ObservableList<Purchase>>() {
+        override fun onItemRangeMoved(p0: ObservableList<Purchase>, p1: Int, p2: Int, p3: Int) = badge.setCount(p0.size)
+        override fun onItemRangeChanged(p0: ObservableList<Purchase>, p1: Int, p2: Int) = badge.setCount(p0.size)
+        override fun onChanged(p0: ObservableList<Purchase>) = badge.setCount(p0.size)
+        override fun onItemRangeInserted(p0: ObservableList<Purchase>, p1: Int, p2: Int) = badge.setCount(p0.size)
+        override fun onItemRangeRemoved(p0: ObservableList<Purchase>, p1: Int, p2: Int) = badge.setCount(p0.size)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = MainActivityViewModel(this)
@@ -42,11 +56,13 @@ class MainActivity : BaseBoundActivity<ActivityMainBinding>(R.layout.activity_ma
         supportFragmentManager.beginTransaction()
                 .replace(R.id.container, binding.vm.dashboardFragment)
                 .commit()
+
+        badge = Badger.sett(binding.bottomNavigation.menu.getItem(2), CountBadge.Factory(this, BadgeShape.circle(0.5f, Gravity.RIGHT.or(Gravity.TOP))))
+        currentCart.purchasesObservableList.addOnListChangedCallback(cartChangeListener)
     }
 
-    override fun onPostResume() {
-        super.onPostResume()
-        val badge = Badger.sett(binding.bottomNavigation.menu.getItem(2), CountBadge.Factory(this, BadgeShape.circle(0.5f, Gravity.RIGHT.or(Gravity.TOP))))
-        badge.setCount(12)
+    override fun onDestroy() {
+        super.onDestroy()
+        currentCart.purchasesObservableList.removeOnListChangedCallback(cartChangeListener)
     }
 }
