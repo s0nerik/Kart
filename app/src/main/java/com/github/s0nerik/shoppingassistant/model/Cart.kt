d711629d01342025b92dc070763f4a35c43ef9f2
Alex Isaienko <thesonerik@gmail.com>
2017-02-26 02:05:09 +0200

    Improved Cart usability.

--- app/src/main/java/com/github/s0nerik/shoppingassistant/model/Cart.kt
+++ app/src/main/java/com/github/s0nerik/shoppingassistant/model/Cart.kt
@@ -1,8 +1,10 @@
 package com.github.s0nerik.shoppingassistant.model
 
+import com.github.s0nerik.shoppingassistant.ext.observableListOf
 import com.github.s0nerik.shoppingassistant.randomUuidString
 import io.realm.RealmList
 import io.realm.RealmObject
+import io.realm.annotations.Ignore
 import io.realm.annotations.PrimaryKey
 import java.util.*
 
@@ -15,4 +17,15 @@ open class Cart(
         @PrimaryKey open var id: String = randomUuidString(),
         open var date: Date = Date(),
         open var purchases: RealmList<Purchase> = RealmList()
-) : RealmObject()
+) : RealmObject() {
+    @Ignore
+    private val lazyPurchases = lazy { observableListOf(purchases) }
+
+    val purchasesObservableList
+        get() = lazyPurchases.value
+
+    fun add(purchase: Purchase) {
+        purchases.add(purchase)
+        purchasesObservableList.add(purchase)
+    }
+}
