e70a95ba7a36a64b8254faf644fede9d32c525e9
Alex Isaienko <thesonerik@gmail.com>
2017-03-04 21:34:02 +0200

    Added transitions to CartFragment

--- app/src/main/java/com/github/s0nerik/shoppingassistant/screens/main/CartFragment.kt
+++ app/src/main/java/com/github/s0nerik/shoppingassistant/screens/main/CartFragment.kt
@@ -1,16 +1,22 @@
 package com.github.s0nerik.shoppingassistant.screens.main
 
 import android.os.Bundle
+import android.support.v4.view.animation.FastOutSlowInInterpolator
+import android.transition.Fade
+import android.transition.Slide
+import android.view.Gravity
 import android.view.View
 import com.github.nitrico.lastadapter.LastAdapter
 import com.github.nitrico.lastadapter.Type
 import com.github.s0nerik.shoppingassistant.BR
 import com.github.s0nerik.shoppingassistant.R
+import com.github.s0nerik.shoppingassistant.anim.Scale
 import com.github.s0nerik.shoppingassistant.base.BaseBoundFragment
 import com.github.s0nerik.shoppingassistant.currentCart
 import com.github.s0nerik.shoppingassistant.databinding.FragmentCartBinding
 import com.github.s0nerik.shoppingassistant.databinding.ItemPurchaseBinding
-import com.github.s0nerik.shoppingassistant.ext.appearScaleIn
+import com.github.s0nerik.shoppingassistant.ext.KTransition
+import com.github.s0nerik.shoppingassistant.ext.KTransitionSet
 import com.github.s0nerik.shoppingassistant.screens.purchase.CreatePurchaseActivity
 import kotlinx.android.synthetic.main.fragment_cart.*
 import org.jetbrains.anko.support.v4.startActivity
@@ -31,6 +37,33 @@ class CartViewModel(val f: CartFragment) {
 class CartFragment : BaseBoundFragment<FragmentCartBinding>(R.layout.fragment_cart) {
     val vm: CartViewModel by lazy { binding.vm }
 
+    init {
+        enterTransition = KTransitionSet.new {
+            ordering(KTransitionSet.Ordering.SEQUENTIAL)
+            transition(Fade(Fade.OUT)) { duration(0); view(R.id.fab) }
+            transitionSet {
+                ordering(KTransitionSet.Ordering.TOGETHER)
+                transitionSet {
+                    view(R.id.recycler)
+                    duration(200)
+                    interpolator(FastOutSlowInInterpolator())
+                    transition(Slide(Gravity.BOTTOM))
+                    transition(Fade(Fade.IN))
+                }
+                transitionSet {
+                    view(R.id.fab)
+                    duration(200)
+                    delay(100)
+                    interpolator(FastOutSlowInInterpolator())
+                    transition(Scale(0.7f))
+                    transition(Fade(Fade.IN))
+                }
+            }
+        }
+
+        exitTransition = KTransition.new(Fade(Fade.OUT)) { duration(200) }
+    }
+
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.vm = CartViewModel(this)
@@ -45,10 +78,5 @@ class CartFragment : BaseBoundFragment<FragmentCartBinding>(R.layout.fragment_ca
 
         recycler.isNestedScrollingEnabled = false
         recycler.setHasFixedSize(true)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        fab.appearScaleIn()
     }
 }
