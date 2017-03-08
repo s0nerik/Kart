ea11e4a6f6e94108d997d427a693f1d1668e7c38
Alex Isaienko <thesonerik@gmail.com>
2017-03-08 20:08:06 +0200

    Improved CreatePurchaseActivity animations.

--- app/src/main/java/com/github/s0nerik/shoppingassistant/screens/product/CreateProductActivity.kt
+++ app/src/main/java/com/github/s0nerik/shoppingassistant/screens/product/CreateProductActivity.kt
@@ -1,5 +1,8 @@
 package com.github.s0nerik.shoppingassistant.screens.product
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
 import android.app.Activity
 import android.content.Intent
 import android.databinding.BaseObservable
@@ -7,8 +10,10 @@ import android.databinding.Bindable
 import android.databinding.ObservableField
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialogFragment
+import android.support.v4.view.animation.FastOutSlowInInterpolator
 import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
+import com.bartoszlipinski.viewpropertyobjectanimator.ViewPropertyObjectAnimator
 import com.github.s0nerik.shoppingassistant.BR
 import com.github.s0nerik.shoppingassistant.R
 import com.github.s0nerik.shoppingassistant.base.BaseBoundActivity
@@ -30,6 +35,7 @@ import kotlinx.android.synthetic.main.item_purchase_preview.view.*
 import kotlinx.android.synthetic.main.view_create_category.*
 import kotlinx.android.synthetic.main.view_create_product.*
 import kotlinx.android.synthetic.main.view_create_shop.*
+import org.jetbrains.anko.dip
 import org.jetbrains.anko.inputMethodManager
 import org.jetbrains.anko.toast
 import java.util.*
@@ -371,5 +377,138 @@ class CreateProductActivity : BaseBoundActivity<ActivityCreateProductBinding>(R.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.vm = CreateProductViewModel(this, realm)
+        animateAppear()
     }
+
+    private fun animateAppear() {
+        val rightButtons = arrayOf(btnSelectName, btnSelectPrice, btnSelectCategory, btnSelectShop)
+        val leftButtons = arrayOf(btnFavorite)
+        val bottomButtons = arrayOf(btnCancelPurchase, btnPurchaseNewProduct)
+
+        val previewAppearDuration = 500L
+        val previewAppearDelay = 200L
+        val buttonsAppearOffset = previewAppearDuration + previewAppearDelay
+
+        root.alpha = 0f
+        root.animate()
+                .alpha(1f)
+                .setDuration(500L)
+                .setInterpolator(FastOutSlowInInterpolator())
+                .start()
+
+        preview.alpha = 0f
+        preview.animate()
+                .alpha(1f)
+                .setDuration(previewAppearDuration)
+                .setStartDelay(previewAppearDelay)
+                .setInterpolator(FastOutSlowInInterpolator())
+                .start()
+
+        rightButtons.forEachIndexed { i, v ->
+            v.translationX = dip(-48).toFloat()
+            v.alpha = 0f
+            v.animate()
+                    .translationX(0f)
+                    .alpha(1f)
+                    .setDuration(200)
+                    .setStartDelay(buttonsAppearOffset + i * 50L)
+                    .start()
+        }
+
+        leftButtons.forEachIndexed { i, v ->
+            v.translationX = dip(48).toFloat()
+            v.alpha = 0f
+            v.animate()
+                    .translationX(0f)
+                    .alpha(1f)
+                    .setDuration(200)
+                    .setStartDelay(buttonsAppearOffset + i * 50L + rightButtons.size * 50L)
+                    .start()
+        }
+
+        bottomButtons.forEachIndexed { i, v ->
+            v.translationY = dip(-24).toFloat()
+            v.alpha = 0f
+            v.animate()
+                    .translationY(0f)
+                    .alpha(1f)
+                    .setDuration(200)
+                    .setStartDelay(buttonsAppearOffset + i * 50L + rightButtons.size * 50L + leftButtons.size * 50L)
+                    .start()
+        }
+    }
+
+    override fun finish() {
+        disappearAndFinish()
+    }
+
+    private fun disappearAndFinish() {
+        val animators = mutableListOf<Animator>()
+
+        val rightButtons = arrayOf(btnSelectName, btnSelectPrice, btnSelectCategory, btnSelectShop).reversed()
+        val leftButtons = arrayOf(btnFavorite).reversed()
+        val bottomButtons = arrayOf(btnCancelPurchase, btnPurchaseNewProduct).reversed()
+
+        val getDelay = { i: Int -> i * 50L }
+
+        bottomButtons.forEachIndexed { i, v ->
+            animators += ViewPropertyObjectAnimator
+                    .animate(v)
+                    .translationY(dip(-24).toFloat())
+                    .alpha(0f)
+                    .setDuration(200)
+                    .setStartDelay(getDelay(i))
+                    .get()
+        }
+
+        leftButtons.forEachIndexed { i, v ->
+            animators += ViewPropertyObjectAnimator
+                    .animate(v)
+                    .translationX(dip(48).toFloat())
+                    .alpha(0f)
+                    .setDuration(200)
+                    .setStartDelay(getDelay(i + bottomButtons.size))
+                    .get()
+        }
+
+        rightButtons.forEachIndexed { i, v ->
+            animators += ViewPropertyObjectAnimator
+                    .animate(v)
+                    .translationX(dip(-48).toFloat())
+                    .alpha(0f)
+                    .setDuration(200)
+                    .setStartDelay(getDelay(i + bottomButtons.size + leftButtons.size))
+                    .get()
+        }
+
+        animators += ViewPropertyObjectAnimator
+                .animate(preview)
+                .alpha(0f)
+                .setDuration(500L)
+                .setInterpolator(FastOutSlowInInterpolator())
+                .setStartDelay(getDelay(rightButtons.size + bottomButtons.size + leftButtons.size))
+                .get()
+
+        animators += ViewPropertyObjectAnimator
+                .animate(root)
+                .alpha(0f)
+                .setDuration(500L)
+                .setInterpolator(FastOutSlowInInterpolator())
+                .setStartDelay(getDelay(rightButtons.size + bottomButtons.size + leftButtons.size) + 200L)
+                .get()
+
+        val set = AnimatorSet()
+        set.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                super@CreateProductActivity.finish()
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                super@CreateProductActivity.finish()
+            }
+        })
+        set.playTogether(animators)
+        set.start()
+    }
+
 }
